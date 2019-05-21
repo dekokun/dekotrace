a008b110af25503af9cd743f5aa3207c4221f489
dekokun <dekokun@hatena.ne.jp>
2019-05-21 13:44:38 +0900

    attach success

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-extern crate nix;
+use libc::pid_t;
 use nix::sys::ptrace;
 use nix::unistd::Pid;
 fn main() {
-    println!("{:?}", ptrace::attach(Pid::this()));
-    println!("{:?}", ptrace::detach(Pid::this()));
+    let pid: pid_t = 19426;
+    println!("{:?}", ptrace::attach(Pid::from_raw(pid)));
+    println!("{:?}", ptrace::detach(Pid::from_raw(pid)));
 }
