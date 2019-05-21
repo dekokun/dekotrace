45910e2149f22530542d65d1f58c9b3f46d2c51b
dekokun <dekokun@hatena.ne.jp>
2019-05-21 13:48:09 +0900

    get pid from commandline argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use libc::pid_t;
 use nix::sys::ptrace;
 use nix::unistd::Pid;
+use std::env;
 fn main() {
-    let pid: pid_t = 19426;
+    let args: Vec<String> = env::args().collect();
+    let pid: pid_t = args[1].parse().unwrap();
     println!("{:?}", ptrace::attach(Pid::from_raw(pid)));
     println!("{:?}", ptrace::detach(Pid::from_raw(pid)));
 }
