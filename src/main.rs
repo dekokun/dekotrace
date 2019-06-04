bbdcb05f8eea22011ffc3f791e64d1f3c6981799
dekokun <dekokun@hatena.ne.jp>
2019-06-04 13:59:33 +0900

    sleepしてattachされたプロセスが止まることを観察

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use libc::pid_t;
 use nix::sys::ptrace;
 use nix::unistd::Pid;
 use std::env;
+use std::thread::sleep;
+use std::{thread, time};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pid: pid_t = args[1].parse().unwrap();
     println!("{:?}", ptrace::attach(Pid::from_raw(pid)));
+    let ten_secs = time::Duration::from_secs(10);
+    thread::sleep(ten_secs);
     println!("{:?}", ptrace::detach(Pid::from_raw(pid)));
 }
